4771d9c26b1d78eee6200a22aed36074937b37e0
Chirantan Patel <chirantan.patel@techvedika.com>
2022-01-06 12:43:19 +0530

    sample example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+//https://www.youtube.com/watch?v=9jULHSe41ls
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './state/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
